af466da7da5f26f3d5b992f815feca30f2a1427b
Isaaczhr <isaaczhrms@outlook.com>
2025-05-27 17:16:44 +0800

    feat: 完成课程资料簿部分全部内容的搬运

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -10,7 +10,7 @@ export default defineUserConfig({
 
   head: [
     // 配置站点图标
-    ['link', { rel: 'icon', type: 'image/png', href: 'logo.svg' }],
+    ['link', { rel: 'icon', href: '/logo.svg' }],
   ],
 
   bundler: viteBundler(),
