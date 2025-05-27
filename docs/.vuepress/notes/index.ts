cbce0bbad333e851541da13e304ffb955f9a45f7
Isaaczhr <isaaczhrms@outlook.com>
2025-05-27 17:46:29 +0800

    feat: 完成生存手册部分全部内容的搬运

--- docs/.vuepress/notes/index.ts
+++ docs/.vuepress/notes/index.ts
@@ -1,6 +1,7 @@
 import { defineNotesConfig } from 'vuepress-theme-plume'
 import admission from './admission'
 import coursedata from './coursedata'
+import survive from './survive'
 
 export default defineNotesConfig({
   // 声明所有笔记的目录，(默认配置，通常您不需要声明它)
@@ -10,5 +11,6 @@ export default defineNotesConfig({
   notes: [ 
     admission,
     coursedata,
+    survive,
   ]
 })
---
+++ docs/.vuepress/notes/survive.ts
@@ -0,0 +1,7 @@
+import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
+
+export default defineNoteConfig({
+  dir: 'survive',
+  link: '/survive',
+  sidebar: 'auto',
+})
